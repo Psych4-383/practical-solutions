576b497cb207403b820e5b8ed3d9f423d42581a0
Psych4-383 <pikachu.bisht.67@gmail.com>
2021-10-18 12:28:55 +0530

    idk

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// window.addEventListener('load', (event) => {
+//     window.scroll(function(){
+//         if(window.scrollTop() > 380){
+//             document.getElementById('img1').style.opacity = '50%'
+//         }
+//     })
+// })
