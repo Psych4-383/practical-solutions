9375a50a1bd3ae3e30b66c9e6b0e63b58986e2c4
Psych4-383 <pikachu.bisht.67@gmail.com>
2021-10-18 13:55:39 +0530

    js animation scroll

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-// window.addEventListener('load', (event) => {
-//     window.scroll(function(){
-//         if(window.scrollTop() > 380){
-//             document.getElementById('img1').style.opacity = '50%'
-//         }
-//     })
-// })
+window.addEventListener('scroll', function(){
+    const currentScroll = window.scrollY;
+    if (Math.ceil(currentScroll) >= 320 && Math.ceil(currentScroll) < 600){
+        const img = this.document.getElementById('img1')
+        const p1 = this.document.getElementById('p1')
+        img.style.transitionDuration = '0.5s'
+        p1.style.transitionDuration = '1s'
+        img.style.width = '40%'
+        p1.style.color = 'black'
+    }
+});
